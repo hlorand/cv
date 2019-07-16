2642b1f847912624885fe74bd919b3d80631ce5c
Lorand Horvath <hlorand@gmail.com>
2019-07-16 13:41:01 +0200

    Projektek elrendezésének módosítása, kevesebb kép, képek tömörítése, readme.

---
+++ readme.md
@@ -0,0 +1,5 @@
+Horváth Loránd önéletrajza.
+
+Legfrissebb változat [letöltése itt](cv.pdf).
+
+A .htm változat nem terjesztésre szánt, ezt alakítom át pdf-é (`./render.sh` -val) így elrendezése furcsa lehet.
--- render.sh
+++ render.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Az oneletrajzomat (cv.htm) alakitja pdf formatumma. Felhasznalja a docker-en keresztul mukodo athenapdf szoftvert.
+
 docker pull arachnysdocker/athenapdf
 
 docker run --rm -v $(pwd):/converted/ arachnysdocker/athenapdf athenapdf -P A3 --no-cache cv.htm cv.pdf
+
+open cv.pdf
